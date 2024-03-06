9393a2224fca5d17ef9d8288596b67fb111ef349
BrenoMatheus <breno.moj99@gmail.com>
2024-03-06 09:22:58 -0300

    feat: atualizado rota da Api

--- frontend/src/shared/environment/index.js
+++ frontend/src/shared/environment/index.js
@@ -1,5 +1,5 @@
 
 export const Environment = {
-  //URL_BASE: 'https://touching-noted-firefly.ngrok-free.app/api',
-  URL_BASE: 'http://localhost:8000/api',
+  URL_BASE: 'https://touching-noted-firefly.ngrok-free.app/api',
+  //URL_BASE: 'http://localhost:8000/api',
 };
